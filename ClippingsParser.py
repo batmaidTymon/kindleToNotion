9b563ed5d78980059faec021acfc1799143264c1
Tymon Terlikiewicz <tymon@batmaid.com>
2022-12-31 15:55:01 +0100

    Import all clippings into new pages

---
+++ ClippingsParser.py
@@ -0,0 +1,37 @@
+import re
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Clipping:
+    author: str
+    title: str
+    highlightText: str
+    dateAndLocation: str
+
+def parseClippings(clippingsFile: str) -> List[Clipping]:
+    clippings = ''
+    # read all content from the clippingsfile
+    with open(clippingsFile, 'r') as file:
+        clippings = file.read().split('==========')
+
+        clippings = [clipping.strip() for clipping in clippings]
+        clippings = list(filter(None, clippings))
+
+    # loop through all clippings
+    for clipping in clippings:
+
+        # get title via regex
+        match = re.search(r'(.*)\((.*)\)', clipping)
+        title = match.group(1)
+        author = match.group(2)
+        highlightText = '\n'.join( clipping.splitlines()[2:] ).strip()
+        dateAndLocation = clipping.splitlines()[1]
+
+
+        yield Clipping(author, title, highlightText, dateAndLocation)
+
+        # Never Split the Difference: Negotiating as if Your Life Depended on It (Chris Voss)
+        # - Your Highlight at location 3749-3751 | Added on Monday, 26 December 2022 10:14:35
+        #
+        # How does this affect the rest of your team? How on board are the people not on this call? What do your colleagues see as their main challenges in this area?
---
+++ main.py
@@ -0,0 +1,94 @@
+import os
+from dataclasses import dataclass
+from notion_client import Client
+from dotenv import load_dotenv
+
+from ClippingsParser import parseClippings
+
+clippingsFile = '/Users/tymon/Library/Mobile Documents/com~apple~CloudDocs/Kindle/My Clippings.txt'
+
+
+def run():
+    notion = Client(auth=os.environ["NOTION_TOKEN"])
+
+    clippings = list(parseClippings(clippingsFile))
+    lastClipping = clippings[-1]
+
+    return syncWithNotion(clippings, notion)
+
+
+def syncWithNotion(clippings, notion: Client):
+    clippingsByTitle = {}
+    for clipping in clippings:
+        if clipping.title not in clippingsByTitle:
+            clippingsByTitle[clipping.title] = []
+
+        clippingsByTitle[clipping.title].append(clipping)
+    parentPage = notion.pages.retrieve(os.environ["NOTION_BOOKS_PAGE_ID"])
+
+    children = notion.blocks.children.list(parentPage['id'])
+    childPageBlocks = [ x for x in children['results'] if x['type'] == 'child_page']
+
+    for title, clippings in clippingsByTitle.items():
+        bookPage = getOrCreateChildPage(notion, parentPage, title, childPageBlocks)
+
+        for clipping in clippings:
+            notion.blocks.children.append(
+                bookPage['id'],
+                children=[
+                    {
+                        "quote": {
+                            "rich_text": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": clipping.highlightText,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        "paragraph": {
+                            "rich_text": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": clipping.dateAndLocation,
+                                    }
+                                }
+                            ],
+                            "color": "gray"
+                        }
+                    }
+                ]
+            )
+    return
+
+
+def getOrCreateChildPage(notion: Client, parentPage: dict[str, object], title, childPageBlocks):
+
+    block = [x for x in childPageBlocks if x['child_page']['title'] == title]
+    if len(block) > 0:
+        return block[0]
+
+    return notion.pages.create(
+        parent={
+            "type": "page_id",
+            "page_id": parentPage['id'],
+        },
+        properties={
+            "title": [
+                {
+                    "text": {
+                        "content": title,
+                    }
+                }
+            ]
+        }
+    )
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    run()
